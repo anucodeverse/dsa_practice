af40edea6bec6a89d07412eed7b7d4ae01dfc8bd
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-24 20:32:31 +0530

    Create ArraysExample.java
    added first dsa program

---
+++ ArraysExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        // Declare and initialize array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array elements
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
