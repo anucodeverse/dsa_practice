d7487b7ba3c54882e1ef20141a16f159b0c4169b
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-25 21:05:18 +0530

    Create bubblesort.java
    insert last element

---
+++ bubblesort.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={6,12,9,3,1};
+		int temp;
+		for(int i=0;i<arr.length-1;i++)
+		{
+		    for(int j=0;j<arr.length-1-i;j++)
+		    if(arr[j]>arr[j+1])
+		    {
+		        temp=arr[j];
+		        arr[j]=arr[j+1];
+		        arr[j+1]=temp;
+		    }
+		   
+		}
+	
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        System.out.println(arr[i]);
+	    }
+	}
+}
