69824dc2fa3b0de372f0f3588886385e5e3b4715
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-25 10:10:37 +0530

    Create sum of array.java
    by using array find sum

---
+++ sum of array.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int sum=0;
+		int arr[]={2,6,4};
+		for(int i=0;i<arr.length;i++)
+		{
+		    sum+=arr[i];
+		}
+		System.out.println(sum);
+	}
+}
