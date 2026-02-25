014de108ee672e19ee1557ce982ca1e85f7629d6
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-25 06:01:24 +0530

    Create foreach.java
    by using foreach to though an elements in array

---
+++ foreach.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[]args)
+    {
+    	int arr[]={5,8,9,10};
+        for(int a:arr)
+        {
+        	System.out.println(a);
+        }
+    }
+}
