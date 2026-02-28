8f95e3a06572620e00e6f629ef7a1e1eacfb6886
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-28 11:23:05 +0530

    Create Multipleparameter.java
    by using multiple parameters to call the method

---
+++ Multipleparameter.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    static void Display(String Name,int age){
+        System.out.println(Name+" is "+age +" years old");
+    }
+	public static void main(String[] args) {
+		Display("anu",21);
+		Display("sri",24);
+		Display("priya",22);
+	}
+}
