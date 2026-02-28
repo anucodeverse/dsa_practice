0ea7999ad63338616f7bd6affaa0dc99539144fa
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-02-28 11:17:35 +0530

    Create MethodParam.java
    by using parameters to call

---
+++ MethodParam.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    static void Display(String Name){
+        System.out.println(Name+" you");
+    }
+	public static void main(String[] args) {
+		Display("I LIKE");
+		Display("I Love");
+		Display("I Hate");
+	}
+}
