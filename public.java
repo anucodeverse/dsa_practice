fdab568e752c64833d6f1cf06d0e6feb3ecfb8bd
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-03-03 17:08:02 +0530

    Create public.java
    using public and private keywords

---
+++ public.java
@@ -0,0 +1,15 @@
+class Method{
+    public String name="anu";
+      private int age=20;
+}
+public class Main
+{
+  
+	public static void main(String[] args) {
+        Method m=new Method();
+    
+        System.out.println(m.name);
+        System.out.println(m.age);
+       
+	}
+}
