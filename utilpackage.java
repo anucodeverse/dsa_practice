df114da6744de23468dd8614ac5a17b2d16c57f1
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-03-03 19:45:45 +0530

    Create utilpackage.java
    scanner class

---
+++ utilpackage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the name:");
+        String name=sc.nextLine();
+        System.out.println("name:"+name);
+    }
+}
